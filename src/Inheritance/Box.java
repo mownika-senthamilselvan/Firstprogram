aebaa5c5f1a0630f5850da4aea1af3d77c695b02
Mowninka <mownikaselvam7@gmail.com>
2023-11-22 17:31:57 +0530

    Inheritance

--- src/Inheritance/Box.java
+++ src/Inheritance/Box.java
@@ -1,4 +1,32 @@
 package Inheritance;
 
 public class Box {
+    protected double l=5;
+    double h;
+    double w;
+     Box()
+    {
+        this.l=-1;
+        this.h=-1;
+        this.w=-1;
+    }
+    Box(double size){
+        this.l= size;
+        this.h= size;
+        this.w= size;
+    }
+
+
+     Box(double l, double h, double w) {
+        this.l = l;
+        this.h = h;
+        this.w = w;
+    }
+    Box(Box box)
+    {
+        this.l = box.l;
+        this.h = box.h;
+        this.w = box.w;
+    }
+
 }
